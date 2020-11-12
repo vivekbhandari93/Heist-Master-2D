61045601d34e427e5e94b72c1beeca255c481efb
VIVEK BHANDARI <ervivekbhandari@gmail.com>
2020-11-12 16:58:33 +0530

    PlayerCharacter scene added.

---
+++ Characters/PlayerCharacter.gd
@@ -0,0 +1,22 @@
+extends "res://Characters/TemplateCharacter.gd"
+
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	move()
+	move_and_slide(velocity)
+
+
+func move():
+	look_at(get_global_mouse_position())
+	if Input.is_action_pressed("down"):
+		velocity.y = clamp(velocity.y + SPEED, 0, MAX_SPEED)
+	elif Input.is_action_pressed("up"):
+		velocity.y = clamp(velocity.y - SPEED, -MAX_SPEED, 0)
+	elif Input.is_action_pressed("right"):
+		velocity.x = clamp(velocity.x + SPEED, 0, MAX_SPEED)
+	elif Input.is_action_pressed("left"):
+		velocity.x = clamp(velocity.x - SPEED, -MAX_SPEED, 0)
+	
+	velocity = lerp(velocity, Vector2.ZERO, FRICTION)
+
--- Characters/TemplateCharacter.gd
+++ Characters/TemplateCharacter.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
 const SPEED = 20
-const MAX_SPEED = 100
+const MAX_SPEED = 200
 const FRICTION = 0.1
 
